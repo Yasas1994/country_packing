79431d371f9da7ae6bb0e29e936f34ebbe7cde2d
Wagner <jonas.wagner@uni-greifswald.de>
2024-09-04 10:26:32 +0200

    Update country_packing.py

--- country_packing.py
+++ country_packing.py
@@ -1,10 +1,32 @@
 import pygame
-import math  # Use the standard math module for trigonometry
-from shapely import Polygon, MultiPolygon, Point
-from shapely.ops import nearest_points
+from shapely import MultiPolygon, Polygon
 import pickle as pkl
 import numpy as np
-from  icecream import ic
+from icecream import ic
+
+
+INITIAL_POS_X = 450
+INITIAL_POS_Y = 300
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 600
+WINDOW_CENTER = np.array([WINDOW_WIDTH/2, WINDOW_HEIGHT/2])
+FRAME_RATE = 30
+SCALING_PARAM = 30
+DEGREES = 5
+COLORS = [
+
+    (255, 0, 0),     # Red
+    (0, 255, 0),     # Green
+    (0, 0, 255),     # Blue
+    (255, 255, 0),   # Yellow
+    (0, 255, 255),   # Cyan
+    (255, 0, 255),   # Magenta
+    (255, 165, 0),   # Orange
+    (128, 0, 128),   # Purple
+    (255, 192, 203),  # Pink
+    (0, 128, 128)    # Teal
+]
+
 
 
 def get_polygon(database, country):
@@ -20,21 +42,22 @@ def get_polygon(database, country):
 
             geom = scaled_polygon.geoms[np.argmax(tmp)]
             x, y = geom.exterior.xy
-            coords = np.array(list(zip(np.array(x), np.array(y)))) 
+            coords = np.array(list(zip(np.array(x),
+                                       np.array(y))))
             ic(geom.centroid.xy)
             centroid = np.array([geom.centroid.xy[0],
-                                         geom.centroid.xy[1]])
+                                 geom.centroid.xy[1]])
         else:
             x, y = scaled_polygon.exterior.xy
             coords = np.array(list(zip(np.array(x), np.array(y))))
             centroid = np.array([scaled_polygon.centroid.xy[0],
-                                         scaled_polygon.centroid.xy[1]])
-            
+                                 scaled_polygon.centroid.xy[1]])
+
         centroid = centroid.reshape(1, 2)
-        scaled_coords = 30*coords
-        scaled_centroid = 30*centroid
+        scaled_coords = SCALING_PARAM*coords
+        scaled_centroid = SCALING_PARAM*centroid
         ic(scaled_centroid)
-        offset = scaled_centroid - np.array([450, 300])
+        offset = scaled_centroid - np.array([INITIAL_POS_X, INITIAL_POS_Y])
         ic(offset)
         corrected_coords = scaled_coords - offset
         corrected_centroid = scaled_centroid - offset
@@ -46,7 +69,7 @@ def get_polygon(database, country):
         raise e
 
 
-def rotate_polygon(coords, theta, centroid, pos):
+def rotate_polygon(coords, theta, centroid):
     # convert degrees to radians
     theta = np.radians(theta)
     # create rotation matrix
@@ -54,8 +77,15 @@ def rotate_polygon(coords, theta, centroid, pos):
                   [np.sin(theta), np.cos(theta)]])
     r = np.round(r, 3)
     # rotate around the centroid
-    return (((coords - centroid) @ r) + centroid) + pos
+    return (((coords - centroid) @ r) + centroid)
 
+
+def text_box(message, position=(900, 10)):
+    text_surface = font.render(f"{message}%",
+                                True, (0,0,0))
+    text_rect = text_surface.get_rect()
+    text_rect.topright = position
+    window.blit(text_surface, text_rect)
 
 def calc_intersect(polgon_1, polygon_2):
     intersected = Polygon(polgon_1).intersection(Polygon(polygon_2))
@@ -69,82 +99,74 @@ def calc_intersect(polgon_1, polygon_2):
 
     return result_polygon
 
-
-def calculate_target_function(current_polygon, small_polygons, big_polygon):
-    # Check if the current_polygon is entirely within the big_polygon
-    ic(current_polygon)
-    ic(small_polygons[0])
-    if not Polygon(big_polygon).contains(Polygon(current_polygon)):
-        return np.inf
-
-    ic(small_polygons[1][0])
-    ic(current_polygon)
-    # Check if current_polygon intersects with any of the small_polygons
-    if Polygon(current_polygon).intersects(Polygon(small_polygons[1][0])): #for small_polygon in small_polygons
-           #if not np.array_equal(current_polygon,small_polygon[0])):
-        ic("Intersect")
-        return np.inf
-
-    # Create a list of all other polygons (excluding the current polygon itself)
-    # We include the exterior of the big_polygon
-    all_other_polygons = [
-        small_polygon for small_polygon in small_polygons
-        if current_polygon != Polygon(small_polygon) and Polygon(small_polygon).intersects(big_polygon)
-    ]
-    all_other_polygons.append(big_polygon.exterior)
-
-    # Convert all_other_polygons to a MultiPolygon for easier distance calculation
-    combined_polygons = MultiPolygon(all_other_polygons)
-
-    # Get the points from the exterior of the current polygon
-    polygon_points = [Point(coord) for coord in current_polygon.exterior.coords]
-
-    # Calculate distances to the nearest point on any of the other polygons
-    distances = [
-        point.distance(nearest_points(point, combined_polygons)[1])
-        for point in polygon_points
-    ]
-
-    # Calculate the mean distance
-    mean_distance = np.mean(distances)
-
-    return mean_distance
-
-
 # Initialize Pygame
 pygame.init()
 font = pygame.font.Font(None, 36)
 # Define the dimensions of the window
-window_width = 900
-window_height = 600
-window = pygame.display.set_mode((window_width, window_height))
-
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 # Set the title of the window
 pygame.display.set_caption("Country_packing")
 
+
+# Polygon class to manage polygon objects
+class Country:
+
+    def __init__(self, coords, centroid, color):
+        self.coords = coords
+        self.color = color
+        self.centroid = centroid
+
+    def draw(self, surface):
+        pygame.draw.polygon(surface, self.color, self.coords)
+
+    def move(self, dx, dy):
+        self.coords = [(x + dx, y + dy) for x, y in self.coords]
+
+    def contains_point(self, point):
+        return pygame.draw.polygon(window, self.color, self.coords, 0).collidepoint(point)
+
+
 polygons = pkl.load(open('./assets/polygons.pkl', 'rb'))
 
-big_country_ = get_polygon(polygons, "Algeria")
-small_countries = [get_polygon(polygons, "Uganda"),
-                   get_polygon(polygons, "Chad")]
-ic(small_countries)
+# get country (coords, centroid)
+big_country_coords = get_polygon(polygons, "Algeria")
+small_country_coords = [get_polygon(polygons, "Uganda"),
+                        get_polygon(polygons, "Sri Lanka"),
+                        get_polygon(polygons, "Palestine"),
+                        get_polygon(polygons, "Israel"),
+                        get_polygon(polygons, "Syria"),
+                        get_polygon(polygons, "South Korea"),
+                        get_polygon(polygons, "Bhutan"),
+                        get_polygon(polygons, "Nepal"),
+                        get_polygon(polygons, "Germany"),
+                        ]
+
+# creaate country objects
+big_country_obj =  [Country(big_country_coords[0], big_country_coords[1], color=(125,125,125))]
+small_country_obj = [Country(i[0], i[1], color=COLORS[index]) for index, i in enumerate(small_country_coords)]
+
+
+ic(big_country_coords[0].shape, small_country_coords[1][1].shape, big_country_coords[0][:, 0].shape)
+
+
 
 # Set initial positions for the shapes
-big_country_pos = [450, 300]  # Starting position of the rectangle
-small_country_pos = [[450, 300], [450,300]]   # Starting position of the triangle
-#small_countr_2_pos = [600, 400]
+big_country_pos = [INITIAL_POS_X, INITIAL_POS_Y]
+# add jitter?
+small_country_pos = [[INITIAL_POS_X, INITIAL_POS_Y] for _ in range(len(small_country_coords))]
+
 # Define a clock to manage the frame rate
 clock = pygame.time.Clock()
 
 # Initialize rotation angle
-rotation_angle = [0, 0]
+rotation_angle = [0 for _ in range(len(small_country_coords))]
 
 # Main loop
 running = True
+rotating = False
 dragging = False
-
-active = 0
-nl = '\n'
+dragged_object_index = 0
+dragged_object = None
 
 
 while running:
@@ -153,18 +175,16 @@ while running:
             running = False
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = event.pos
-            # Check if the mouse is clicking inside the triangle
-            if small_countries_rect[0].collidepoint(mouse_x, mouse_y):
-                dragging = True
-                mouse_offset_x = small_countries_rect[0].x - mouse_x
-                mouse_offset_y = small_countries_rect[0].y - mouse_y
-                active = 0
-            elif small_countries_rect[1].collidepoint(mouse_x, mouse_y):
-                dragging = True
-                mouse_offset_x = small_countries_rect[1].x - mouse_x
-                mouse_offset_y = small_countries_rect[1].y - mouse_y
-                active = 1
+            rotating = False
+            for index, obj in enumerate(small_country_obj):
+                if obj.contains_point(event.pos):
+                    dragging = True
+                    dragged_object = obj
+                    dragged_object_index = index
+                    mouse_offset_x_coord = obj.coords[:, 0] - event.pos[0]
+                    mouse_offset_y_coord  = obj.coords[:, 1] - event.pos[1]
+                    mouse_offset_x_centroid = obj.centroid[:, 0] - event.pos[0]
+                    mouse_offset_y_centroid = obj.centroid[:, 1] - event.pos[1]
 
         elif event.type == pygame.MOUSEBUTTONUP:
             dragging = False
@@ -172,69 +192,58 @@ while running:
         elif event.type == pygame.MOUSEMOTION:
             if dragging:
                 mouse_x, mouse_y = event.pos
-                small_country_pos[active][0] = mouse_x + mouse_offset_x - 450
-                small_country_pos[active][1] = mouse_y + mouse_offset_y - 300
+                # update x_position of the object
+                dragged_object.coords[:, 0] = event.pos[0] + mouse_offset_x_coord #- INITIAL_POS_X
+                # update the y position of the object
+                dragged_object.coords[:, 1] = event.pos[1] + mouse_offset_y_coord #- INITIAL_POS_Y
 
+                dragged_object.centroid[:, 0] = event.pos[0] + mouse_offset_x_centroid #- INITIAL_POS_X
+                dragged_object.centroid[:, 1] = event.pos[1] + mouse_offset_y_centroid #- INITIAL_POS_Y
 
         elif event.type == pygame.MOUSEWHEEL:
-            rotation_angle[active] += 5  # Adjust rotation speed as needed
+            rotating = True
+            if event.y > 0:
+                rotation_angle[dragged_object_index] = (np.abs(rotation_angle[dragged_object_index]) + event.y)*1
+            elif event.y < 0:
+                # ic(event.y + -1 * (rotation_angle[dragged_object_index]))
+                rotation_angle[dragged_object_index] = (event.y + -1 * np.abs(rotation_angle[dragged_object_index]))*1
+
 
     # Clear the screen with a white background
-    window.fill((255, 255, 255))
+    window.fill((215, 215, 215))
+
+    # draw objects
+    big_country_obj[0].draw(window)
+    for s_obj in small_country_obj:
+        # ic(s_obj)
+        s_obj.draw(window)
 
     # Rotate the triangle
-    rotated_small_country = rotate_polygon(small_countries[0][0],
-                                           rotation_angle[0],
-                                           small_countries[0][1],
-                                           small_country_pos[0])
-    rotated_small_country_2 = rotate_polygon(small_countries[1][0],
-                                           rotation_angle[1],
-                                           small_countries[1][1],
-                                           small_country_pos[1])
-    #ic(rotated_small_country)
-    # Draw the rectangle and the rotated triangle
-    pygame.draw.polygon(window, (0, 119, 182), big_country_[0])  # Blue rectangle
-    pygame.draw.polygon(window, (207, 142, 128), rotated_small_country)  # Red triangle
-    pygame.draw.polygon(window, (252, 181, 181), rotated_small_country_2)  # Red triangle
-
-    overlap_1 = np.round(Polygon(big_country_[0]).intersection(Polygon(rotated_small_country)).area/Polygon(rotated_small_country).area*100, 1)
-    overlap_2 = np.round(Polygon(big_country_[0]).intersection(Polygon(rotated_small_country_2)).area/Polygon(rotated_small_country_2).area*100, 1)
-    overlap_b = np.round(Polygon(rotated_small_country).intersection(Polygon(rotated_small_country_2)).area/Polygon(rotated_small_country_2).area*100, 1)
-    text_surface = font.render(f"{overlap_1}%",
-                               True, (0,0,0))
-    text_rect = text_surface.get_rect()
-    text_rect.topright = (900, 10)
-    window.blit(text_surface, text_rect)
-    text_surface = font.render(f"{overlap_2}%",
-                               True, (0, 0, 0))
-    text_rect = text_surface.get_rect()
-    text_rect.topright = (900, 30)
-    window.blit(text_surface, text_rect)
-    text_surface = font.render(f"{overlap_b}%",
-                               True, (0, 0, 0))
-    text_rect = text_surface.get_rect()
-    text_rect.topright = (900, 50)
-    window.blit(text_surface, text_rect)
-
-    if Polygon(rotated_small_country).intersects(Polygon(rotated_small_country_2)):
-        intersected = calc_intersect(rotated_small_country, rotated_small_country_2)
-
-        pygame.draw.polygon(window, (255, 23, 25), intersected)
-        # Update the triangle's rect for collision detection
-    small_countries_rect = []
-    small_countries_rect.append(pygame.draw.polygon(window,
-                                             (255, 0, 0),
-                                             rotated_small_country, 1))
-    small_countries_rect.append(pygame.draw.polygon(window,
-                                                  (255, 0, 0),
-                                                  rotated_small_country_2, 1))
-
-   # calculate_target_function(small_countries[active][0], small_countries, Polygon(big_country_[0]))
+    if (dragged_object is not None) and rotating:
+        ic(dragged_object_index, rotation_angle)
+        rotated_small_country = rotate_polygon(coords=dragged_object.coords,
+                                               theta=rotation_angle[dragged_object_index],
+                                               centroid=dragged_object.centroid)
+        # ic(rotated_small_country)
+        # Draw the rectangle and the rotated triangle
+        dragged_object.coords = rotated_small_country
+        dragged_object.draw(window)
+        rotating = False
+    # overlap = np.round(Polygon(big_country_[0]).intersection(Polygon(rotated_small_country)).area/Polygon(rotated_small_country).area*100, 1)
+    for i, _ in enumerate(small_country_obj):
+        overlap = np.round(Polygon(big_country_obj[0].coords).intersection(Polygon(small_country_obj[i].coords)).area/Polygon(small_country_obj[i].coords).area*100, 1)
+        text_box(overlap, position=(900, 10+i*20))
+        for j, _ in enumerate(small_country_obj):
+             if Polygon(small_country_obj[i].coords).intersects(Polygon(small_country_obj[j].coords)):
+                 if i != j:
+                    intersected = calc_intersect(small_country_obj[i].coords, small_country_obj[j].coords)
+        #         ic(intersected)
+                    pygame.draw.polygon(window, (0, 0, 0), intersected)
     # Update the display
     pygame.display.flip()
 
     # Cap the frame rate at 30 FPS
-    clock.tick(30)
+    clock.tick(FRAME_RATE)
 
 # Quit Pygame
 pygame.quit()
